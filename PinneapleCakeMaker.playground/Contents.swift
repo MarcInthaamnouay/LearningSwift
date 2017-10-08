1bfad8948cd269573e42302f81451a58e8080074
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-10-09 00:05:34 +0200

    WIP constructing pineapple cake

--- PinneapleCakeMaker.playground/Contents.swift
+++ PinneapleCakeMaker.playground/Contents.swift
@@ -16,6 +16,14 @@ struct Const {
         "kaoshiung": ["舊振南", "呷百二"],
         "keelung"  : ["李鵠"]
     ]
+    
+    // Ranking of some manufacturer
+    static var manufacturerRanking: [String: Int] = [
+        "佳德": 7,
+        "新東陽": 7,
+        "佳德": 10,
+        "台北犁記": 8
+    ]
 }
 
 extension Const {
@@ -28,9 +36,9 @@ extension Const {
         name: the name of the company
      
      - returns:
-        
+     
      */
-    mutating func addManufacturer(city: String, name: String) {
+    static func addManufacturer(city: String, name: String) {
         // Check if the city is in the list of the manufacturer
         let presence = Const.manufacturerList.filter{$0.key == city}
         
@@ -87,6 +95,25 @@ enum ManufacturerList: String {
     case kaohsiung
     
     /**
+     Return the ranking of a pinneaple cake manufacturer bsased on the name of the company
+     
+     - paramters:
+        - name: The name of the company
+     
+     - returns:
+        - An optional integer (nil if not present)
+     */
+    private func getRanking(name: String) -> Int? {
+        let rank = Const.manufacturerRanking.filter{$0.key == name}
+        
+        if (rank.count == 0) {
+            return nil
+        }
+        
+        return rank[0].value
+    }
+    
+    /**
      Return a manufacturer information based on the name and the city
      
      - parameters:
@@ -98,15 +125,61 @@ enum ManufacturerList: String {
      In the case of an unknown manufacturer is pass this method will throw
      
      */
-    func getManufacturerByDistrict(name: String) throws -> (name: String, city: String){
+    func getManufacturerByName(name: String) throws -> Manufacturer {
         let collection = Const.manufacturerList[self.rawValue]
         let manName = collection?.filter{$0 == name}
         
-        if (manName == nil) {
+        if (manName?.count == 0) {
            throw PineappleError.unknownManufacturer
         }
         
-        return (manName![0], self.rawValue)
+        return Manufacturer(
+            name: manName![0],
+            city: self.rawValue,
+            ranking: self.getRanking(name: manName![0])
+        )
+    }
+    
+    /**
+     Return all the manufacturer in a city
+     
+     - returns
+        - Array of String with the name of the manufacturer
+     */
+    func getAllManufacturerByCity() -> [String] {
+        return Const.manufacturerList[self.rawValue]!
+    }
+}
+
+enum BasicIngredient {
+    case butter
+    case eggYolk
+    case sugar
+    case flour
+    case honey
+    case cornSyrup
+    
+    /**
+     Return a basic Ingredient based on this enum
+     
+     - returns:
+        Ingredient value
+     */
+    func getBasicIngredient() -> Ingredient {
+        switch self {
+        case .butter:
+            return Ingredient(name: "Butter", calories: 1290.0)
+        case .eggYolk:
+            return Ingredient(name: "Egg Yolk", calories: 110.0)
+        case .sugar:
+            return Ingredient(name: "Sugar", calories: 232.0)
+        case .flour:
+            return Ingredient(name: "Flour", calories: 915.0)
+        case .honey:
+            return Ingredient(name: "Honey", calories: 257.75)
+        case .cornSyrup:
+            return Ingredient(name: "Corn Syrup", calories: 482.5)
+        }
     }
 }
 
@@ -118,8 +191,8 @@ struct Pineapple {
 
 struct Manufacturer {
     var name   : String
-    var country: String
-    var ranking: UInt8
+    var city   : String
+    var ranking: Int?
 }
 
 struct Ingredient {
@@ -130,8 +203,26 @@ struct Ingredient {
 struct Cake {
     var pinneappleType: Pineapple
     var manufacturer  : Manufacturer
-    var ingredientList: [Ingredient]
+    var ingredientList: [Ingredient]?
+    
+    /**
+     Init: We override the init as we want to have already some properties in the ingredientList
+     
+     - parameters: 
+        - type: Pinneaple, a type of pineapple
+        - seller: A manufacturer
+     */
+    init(type: Pineapple, seller: Manufacturer) {
+        self.pinneappleType = type
+        self.manufacturer = seller
+        self.ingredientList = [
+            BasicIngredient.butter.getBasicIngredient(),
+            BasicIngredient.eggYolk.getBasicIngredient(),
+            BasicIngredient.flour.getBasicIngredient()
+        ]
+    }
 }
 
 // UI below
+
 
